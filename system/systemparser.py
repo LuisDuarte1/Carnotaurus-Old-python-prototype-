180fb29a19bc098cd62e8a9fea76fe19a208389a
Luís Duarte <sr.zitinho@hotmail.com>
2019-03-09 10:59:40 +0000

    Started making a parser only for system related functions

---
+++ system/systemparser.py
@@ -0,0 +1,19 @@
+import logging
+import queue
+import threading
+
+logger = logging.getLogger(__name__)
+
+class SystemParser(threading.Thread):
+    """
+        A sub-parser to process system related functions, usually from the server.
+    """
+
+    def __init__(self):
+        self.queuerecv = queue.Queue()
+        self.start()
+
+    def run(self):
+        while True:
+            data = self.queuerecv.get() #On a sub-parser the data must be decrypted and in a dictionary to easier use
+            
