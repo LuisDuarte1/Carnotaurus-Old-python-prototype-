0ea484b59d24352e7738ee88fcce81e8ebfa31eb
Luís Duarte <sr.zitinho@hotmail.com>
2019-03-16 11:18:42 +0000

    Added support for inter parser communication on SystemParser

--- system/systemparser.py
+++ system/systemparser.py
@@ -1,6 +1,7 @@
 import logging
 import queue
 import threading
+import variables
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +13,14 @@ class SystemParser(threading.Thread):
     def __init__(self, interparserqueue):
         super().__init__()
         self.queuerecv = interparserqueue
+        threading.Thread(target=self.GetInterParserQueue).start()
         self.start()
+
+    def GetInterParserQueue(self):
+        while variables.parser_poolobj == None: #Check if parser_pool obj is initialized
+            continue
+        self.parser_comms = variables.parser_poolobj.InterParserQueue
+        logger.debug("Got the interparser queue...")
 
     def run(self):
         while True:
