64f6ce173fd753b52af6de64086d719a73441f47
Luís Duarte <sr.zitinho@hotmail.com>
2019-04-07 00:56:27 +0100

    Added get_all_stats action for clients

--- system/systemparser.py
+++ system/systemparser.py
@@ -2,6 +2,7 @@ import logging
 import queue
 import threading
 import sys
+from system import GetCpuUsage, GetFreeRam, GetProcessList, GetSpecs
 import time
 sys.path.append('../')
 import json
@@ -14,7 +15,7 @@ class SystemParser(threading.Thread):
         A sub-parser to process system related functions, usually from the server.
     """
 
-    deltatime_stats = 600
+    deltatime_stats = 10
 
     def __init__(self, interparserqueue):
         super().__init__()
@@ -32,7 +33,7 @@ class SystemParser(threading.Thread):
         logger.debug("Got the interparser queue...")
 
     def GetStatsFromAllClients(self):
-        self.parser_comms.put({'to': 'networking_server', 'action':'sendall', 'to_send':"{'type':'system', 'action':'get_all_stats'}"})
+        self.parser_comms.put({'to': 'networking_server', 'action':'sendall', 'to_send':'{"type":"system", "action":"get_all_stats"}'})
         threading.Timer(self.deltatime_stats ,self.GetStatsFromAllClients).start()
 
 
@@ -41,5 +42,15 @@ class SystemParser(threading.Thread):
             data = self.queuerecv.get() #On a sub-parser the data must be decrypted and in a dictionary to easier use
             if variables.typee == 'SERVER': #Process exclusive data from the clients
                 pass
-            else: #Process exclusive data from the server or general data
-                pass
+            elif variables.typee == 'CLIENT': #Process exclusive data from the server
+                if data['action'] == 'get_all_stats':
+                    stats = {
+                        'cpu_usage': GetCpuUsage(2),
+                        'free_ram': GetFreeRam(),
+                        'process_list': GetProcessList()
+                    }
+                    self.parser_comms.put({
+                        'to': 'networking_client',
+                        'action': 'sendserver',
+                        'to_send': json.dumps({'type':"system", 'action':'process_stat_data', 'data':stats})
+                        })
