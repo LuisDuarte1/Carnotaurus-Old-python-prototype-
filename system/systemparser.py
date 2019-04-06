077c15edc0dce4b5dce72cb52568ba8d359816a2
Luís Duarte <sr.zitinho@hotmail.com>
2019-04-06 23:17:51 +0100

    Added a server function to get client system stats periodically

--- system/systemparser.py
+++ system/systemparser.py
@@ -1,6 +1,10 @@
 import logging
 import queue
 import threading
+import sys
+import time
+sys.path.append('../')
+import json
 import variables
 
 logger = logging.getLogger(__name__)
@@ -10,10 +14,15 @@ class SystemParser(threading.Thread):
         A sub-parser to process system related functions, usually from the server.
     """
 
+    deltatime_stats = 600
+
     def __init__(self, interparserqueue):
         super().__init__()
         self.queuerecv = interparserqueue
+        self.parser_comms = None
         threading.Thread(target=self.GetInterParserQueue).start()
+        if variables.typee == 'SERVER':
+            threading.Timer(self.deltatime_stats ,self.GetStatsFromAllClients).start()
         self.start()
 
     def GetInterParserQueue(self):
@@ -22,7 +31,15 @@ class SystemParser(threading.Thread):
         self.parser_comms = variables.parser_poolobj.InterParserQueue
         logger.debug("Got the interparser queue...")
 
+    def GetStatsFromAllClients(self):
+        self.parser_comms.put({'to': 'networking_server', 'action':'sendall', 'to_send':"{'type':'system', 'action':'get_all_stats'}"})
+        threading.Timer(self.deltatime_stats ,self.GetStatsFromAllClients).start()
+
+
     def run(self):
         while True:
             data = self.queuerecv.get() #On a sub-parser the data must be decrypted and in a dictionary to easier use
-            
+            if variables.typee == 'SERVER': #Process exclusive data from the clients
+                pass
+            else: #Process exclusive data from the server or general data
+                pass
