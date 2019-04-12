c3ed9c73237bf8d6706a2a711c9ae8daadf6b077
Luís Duarte <sr.zitinho@hotmail.com>
2019-04-12 12:14:26 +0100

    Changed MainConfig to allow to specify the path

--- config/mainconfig.py
+++ config/mainconfig.py
@@ -10,21 +10,22 @@ logger = logging.getLogger(__name__) #Get logger from main
 
 class MainConfig:
 
-    def __init__(self):
-        if os.path.isfile("config.json") == False:
+    def __init__(self, path="config.json"):
+        self.path = path
+        if os.path.isfile(path) == False:
             self.FirstRun()
         self.LoadConfig()
         variables.config_obj = self
 
     def FirstRun(self):
-        with open("config.json", "w") as f: #Open the config file, overriding if the config already exists
+        with open(self.path, "w") as f: #Open the config file, overriding if the config already exists
             configdict = {}
             configdict['mode'] = 'client' #Set carnotaurus as client by default, you can change this by using the argument --server
             configdict['uuid'] = str(uuid.uuid4()) #Create a new uuid for this carnotaurs client/server
             f.write(json.dumps(configdict))
 
     def LoadConfig(self):
-        self.configdict = json.load(open("config.json", "r")) #Get dict by using json.load because it does the transformation between string and dict
+        self.configdict = json.load(open(self.path, "r")) #Get dict by using json.load because it does the transformation between string and dict
     
     def GetConfigArgument(self, key):
         try:
@@ -41,5 +42,5 @@ class MainConfig:
     
 
     def CommitToConfigFile(self):
-        with open("config.json", "w") as f: #This will override the old config 
+        with open(self.path, "w") as f: #This will override the old config 
             f.write(json.dumps(self.configdict))
